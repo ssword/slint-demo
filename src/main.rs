cc69f81ee8ffaae396592ac8d475cc6423252d25
ssword <ssword@gmail.com>
2023-04-07 00:15:53 +0800

    extended the tiles to a grid of 16 tiles

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    use slint::Model;
+
+    let main_window = MainWindow::new().unwrap();
+
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix  the tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    // Assign the shuffled Vec to the model property
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.into());
+    main_window.run().unwrap();
 }
 
 slint::slint! {
+
+    struct TileData {
+        image: image,
+        image_visible: bool,
+        solved: bool,
+    }
     component MemoryTile inherits Rectangle {
         callback clicked;
         in property <bool> open_curtain;
@@ -48,12 +71,32 @@ slint::slint! {
     }
 
     export component MainWindow inherits Window {
-        MemoryTile {
-            icon: @image-url("icons/bus.png");
+        width: 326px;
+        height: 326px;
+
+        in property <[TileData]> memory_tiles: [
+            { image: @image-url("icons/at.png") },
+            { image: @image-url("icons/balance-scale.png") },
+            { image: @image-url("icons/bicycle.png") },
+            { image: @image-url("icons/bus.png") },
+            { image: @image-url("icons/cloud.png") },
+            { image: @image-url("icons/cogs.png") },
+            { image: @image-url("icons/motorcycle.png") },
+            { image: @image-url("icons/video.png") },
+        ] ;
+
+        for tile[i] in memory_tiles: MemoryTile {
+            x: mod(i, 4) * 74px;
+            y: floor(i / 4) * 74px;
+            width: 64px;
+            height: 64px;
+            icon: tile.image;
+            open_curtain: tile.image_visible || tile.solved;
+            // propagate the solved status from the model to the tile
+            solved: tile.solved;
             clicked => {
-                self.open_curtain = !self.open_curtain;
+                tile.image_visible = !tile.image_visible;
             }
         }
     }
-
 }
